1559d6741cf3a07aed621193652815e3dd4086a1
Srivatsa Narasimha <dataquirky89@gmail.com>
2026-03-30 15:02:40 +0530

    Remove variable name from problem statement
    Simplify the problem description by removing the explicit variable name 'strs' (changed to 'an array of strings') to make the wording more generic and readable.

--- arrays-hashing/Medium/049_group_anagrams.py
+++ arrays-hashing/Medium/049_group_anagrams.py
@@ -7,7 +7,7 @@ Primary Pattern: Arrays + Hashing
 Secondary: Frequency Count
 
 Problem Statement:
-Given an array of strings strs, group the anagrams together.
+Given an array of strings, group the anagrams together.
 You can return the answer in any order.
 
 --------------------------------------------------
